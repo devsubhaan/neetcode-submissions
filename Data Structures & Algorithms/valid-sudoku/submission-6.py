d60b1b6642a4d3caebad4f2d0b2d6e00fc82ba95
Subhaan <subhaanahmed514@gmail.com>
2026-09-01 13:48:14 +0100

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        l = len(board)
+
+        for i in range(l):
+            for val in board[i]:
+                if not val in ["1","2","3","4","5","6","7","8","9","."]: #check if its valid num or empty
+                    return False
+            
+            #check horizontal rows
+            nrow = list(filter(lambda x: x != ".", board[i])) 
+            if len(nrow) != len(set(nrow)):
+                return False
+
+            #check vertical rows
+            tmp = set()
+            for j in range(l):
+                if board[j][i] == ".":
+                    continue
+
+                if board[j][i] in tmp:
+                    return False
+
+                tmp.add(board[j][i])
+
+        #check each 3x3 grid
+        for br in range(0,9,3):
+            for bc in range(0,9,3):
+
+                vals = set()
+
+                for r in range(br,br+3):
+                    for c in range(bc,bc+3):
+                        if board[r][c] == ".":
+                            continue
+
+                        if board[r][c] in vals:
+                            return False
+
+                        vals.add(board[r][c])
+
+        return True
