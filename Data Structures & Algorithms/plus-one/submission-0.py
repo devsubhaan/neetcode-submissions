29c0151ad1854936d7075c00405e1866f8663188
Subhaan <subhaanahmed514@gmail.com>
2026-09-20 16:40:21 +0100

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        l = len(digits)-1
+        for i in range(l,-1,-1):
+            if digits[i] < 9:
+                digits[i] +=1
+                return digits
+            else:
+                digits[i] = 0
+
+        return [1] + digits
+        
